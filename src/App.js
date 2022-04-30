c158f3919c8ccfc63ec8173abab67e8b57e54e3b
каримов расулбек <rasulbekkari008@gmail.com>
2022-04-30 19:23:45 +0600

    async qyery

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
 import Modal from "./components/Modal";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { getCartItems } from "./features/cart/cartSlice";
+import { useEffect } from "react";
 function App() {
   const modal = useSelector(state => state.model.isOpen)
+  const isLoading = useSelector(state => state.isLoading)
+  const dispatch = useDispatch();
   console.log(modal)
+
+
+  useEffect(() => {
+    dispatch(getCartItems())
+  }, [])
+
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>loading</h1>
+      </div>
+    )
+  }
   return <main>
     {modal && <Modal />}
     <Navbar />
